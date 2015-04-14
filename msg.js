ed6e875018756c2e30b123698035f37ddc1a3dbb
Soonthorn <harmonst@yahoo.com>
2015-04-14 13:54:32 -0700

    Create msg.js
    jscode

---
+++ msg.js
@@ -0,0 +1,29 @@
+//msg.js
+//By: SCG
+
+function nmsg() {
+	document.getElementById("messages").innerHTML=
+	"First Name: 4-30 Chars, UPPER/lower case and - or ' only!";
+}
+
+function lmsg() {
+	document.getElementById("messages").innerHTML=
+	"Last Name: 4-30 Chars, UPPER/lower case and - or ' only!";
+}
+
+
+function emailmsg() {
+	document.getElementById("messages").innerHTML=
+	"Email: Must be Valid email address";
+}
+
+
+function phonemsg() {
+	document.getElementById("messages").innerHTML=
+	"Phone Number: Must be Valid Phone Number";
+}
+
+function commsg() {
+	document.getElementById("messages").innerHTML=
+	"Enter Your Comments Here";
+}
